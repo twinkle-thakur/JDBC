46143f3f29000d8d523edbf7baa3aa024d3c12c7
twinkle-thakur <twinklethakur7879@gmail.com>
2025-05-10 16:58:32 +0530

    resultset

--- jdbcPractice/src/com/jdbc/metadata/ResultSetInformation.java
+++ jdbcPractice/src/com/jdbc/metadata/ResultSetInformation.java
@@ -24,13 +24,15 @@ public static void main(String[] args) throws SQLException {
 	ResultSet resultSet = statement.executeQuery("select * from student");
 	ResultSetMetaData metaData = resultSet.getMetaData();
 	String catalogName = metaData.getCatalogName(1);
-	System.out.println(catalogName);
-	System.out.println(metaData.getColumnCount());
+	System.out.println(catalogName); //gives database name
+	System.out.println(metaData.getColumnCount()); //no. of col in table
 	String columnClassName = metaData.getColumnClassName(2);
 	System.out.println(columnClassName);
-	System.out.println(metaData.getColumnDisplaySize(3));
+	System.out.println(metaData.getColumnDisplaySize(3)); //col var size
 	String columnLabel = metaData.getColumnLabel(2);
-	System.out.println(columnLabel);
-	System.out.println(metaData.getColumnName(2));
+	System.out.println(columnLabel); //gives col name
+	System.out.println(metaData.getColumnName(2)); //gives col name
+	System.out.println(metaData.getColumnType(1)); //size of datatype
+	System.out.println(metaData.getColumnTypeName(1));//data type
 }
 }
